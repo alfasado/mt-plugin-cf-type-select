66f0e7026edc1b3356a8ad63d3521258f173710a
Alfasado Inc <webmaster@alfasado.jp>
2013-04-19 12:32:19 +0900

    first commit

--- pligins/CFTypeSelect/lib/CFTypeSelect/Field.pm
+++ pligins/CFTypeSelect/lib/CFTypeSelect/Field.pm
@@ -7,7 +7,7 @@ sub _override_customfield_types {
     my $customfield_types = MT->registry( 'customfield_types' );
     my $select_field = $customfield_types->{ 'select' };
     my $delimiter = MT->config( 'CFDropDownDelimiter' );
-    $select_field->{ 'options_delimiter' } = MT->config( 'CFDropDownDelimiter' );
+    $select_field->{ 'options_delimiter' } = $delimiter;
     my $options_field = '<input type="text" name="options" value="<mt:var name="options" escape="html">" id="options" class="text" /><p class="hint">';
     $options_field .= $plugin->translate( "Please enter all allowable options for this field as a [_1] delimited list", $delimiter );
     $options_field .= '</p>';
